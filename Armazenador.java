8da508496a4174f2dd6f7280113527e35749687c
Kauã Bezerra Brito <ra00364001@pucsp.edu.br>
2026-03-19 12:19:50 -0300

    bugfix: add remove function again

--- Armazenador.java
+++ Armazenador.java
@@ -28,7 +28,18 @@ public class Armazenador implements IArmazenador {
     }
 
     public boolean remover(String ra) {
-        return true;
+        // itera sobre o armazenador
+    	for (int i = 0; i < arm.length; i++) {
+    		// checa se, p/ aquela posição, o aluno existe e se o RA é igual ao RA buscado.
+    		if (arm[i] != null && arm[i].getRa().equals(ra)) {
+    			// se for, atribui null
+    			arm[i] = null;
+    			// retorna true
+    			return true;
+    		}
+    	}
+        // retorna falso por padrão
+        return false;
     }
     
     public void listar() {
