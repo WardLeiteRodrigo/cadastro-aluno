8ba3e6e057a53a5674e970b2d8a51829de0651fe
Kauã Bezerra Brito <ra00364001@pucsp.edu.br>
2026-03-19 12:12:48 -0300

    feat: Create Armazenador's listar function

--- Armazenador.java
+++ Armazenador.java
@@ -10,12 +10,31 @@ public class Armazenador implements IArmazenador {
     }
 
     public boolean inserir(Aluno a) {
-        // TODO
+        int i = 0;
+        int tam = arm.length;
+        boolean ctrl = false;
+        
+        while(ctrl != true && i < tam) {
+            
+            if(arm[i] == null) {
+                arm[i] = a;
+                ctrl = true;
+            }
+            
+            i++;
+        }
         return true;
     }
 
-    public boolean remover (String ra) {
-        // TODO
+    public boolean remover(String ra) {
         return true;
+    }
+    
+    public void listar() {
+        for (int i = 0; i < arm.length; i++) {
+            if (arm[i] != null) {
+                System.out.printf("%s\n", arm[i].ra);
+            }
+        }
     }
 }
