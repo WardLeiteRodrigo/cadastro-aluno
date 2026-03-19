1fbc1490790e5d7a36e1c4ee954d0389e19077a5
Liam Vedovato Lopes <ra00359597@pucsp.edu.br>
2026-03-19 12:08:54 -0300

    feat: create getRa function on Aluno class fix: adjust Armazenador class indentation

--- Armazenador.java
+++ Armazenador.java
@@ -29,18 +29,18 @@ public class Armazenador implements IArmazenador {
         return true;
     }
 
-	public boolean remover(String ra) {
-		// itera sobre o armazenador
-		for (int i = 0; i < arm.length; i++) {
-			// checa se, p/ aquela posição, o aluno existe e se o RA é igual ao RA buscado.
-			if (arm[i] != null && arm[i].getRa().equals(ra)) {
-				// se for, atribui null
-				arm[i] = null;
-				// retorna true
-				return true;
-			}
-		}
-	// retorna falso por padrão
+    public boolean remover(String ra) {
+    	// itera sobre o armazenador
+    	for (int i = 0; i < arm.length; i++) {
+    		// checa se, p/ aquela posição, o aluno existe e se o RA é igual ao RA buscado.
+    		if (arm[i] != null && arm[i].getRa().equals(ra)) {
+    			// se for, atribui null
+    			arm[i] = null;
+    			// retorna true
+    			return true;
+    		}
+    	}
+    // retorna falso por padrão
     return false;
-	}
+    }
 }
