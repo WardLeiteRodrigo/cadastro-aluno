218501c508acc269ddd1c57b0a5a70a2917a15c9
liamlopes756 <liamlopes2004@gmail.com>
2026-03-24 10:28:25 -0300

    feat: add inserir, remover and listar on menu

--- App.java
+++ App.java
@@ -19,7 +19,33 @@ public class App {
         
         do {
             opcao = mn.criarMenu(itensMenu);
-
+            
+            switch(opcao) {
+                case 1: 
+                    String nome; int idade;
+                    String ra; String curso; int semestre;
+                    
+                    nome = JOptionPane.showInputDialog("Nome: ");
+                    idade = Integer.parseInt(JOptionPane.showInputDialog("Idade: "));
+                    ra = JOptionPane.showInputDialog("RA: ");
+                    curso = JOptionPane.showInputDialog("Curso: ");
+                    semestre = Integer.parseInt(JOptionPane.showInputDialog("Semestre: "));
+                    
+                    Aluno a = new Aluno(nome, idade, ra, curso, semestre);
+                    ca.inserir(a);
+                    
+                    break;
+                case 2:
+                    String ra_remover = JOptionPane.showInputDialog("RA a ser removido: ");
+                    ca.remover(ra_remover);
+                    
+                    break;
+                case 3: 
+                    ca.listar();
+                    break; 
+                case 4: break;
+                default:
+            }
         } while(opcao != 4);
     }
 }
