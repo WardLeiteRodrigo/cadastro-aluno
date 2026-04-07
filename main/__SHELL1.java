2704a47d3841cc32367b2d5034c4f3392769a4a5
raulkolaric <raul.kolaric@gmail.com>
2026-04-07 09:53:54 -0300

    remove gemini.md
    Co-authored-by: liamlopes756 <liamlopes2004@gmail.com>
    Co-authored-by: KauaBezerraBrito <kaua.bezerra2006@gmail.com>
    Co-authored-by: WardLeiteRodrigo <leitewrodrigo@hotmal.com>

---
+++ main/__SHELL1.java
@@ -0,0 +1,8 @@
+package main;
+public class __SHELL1 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+main.App.main(__bluej_param0);
+
+}}
